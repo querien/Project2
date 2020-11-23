925bcda8ec8606cf89d8d382b3b1c5c9e9d67ab6
Rosana Batista <rosana.psb@gmail.com>
2020-11-23 16:09:19 +0100

    create profile page with buttons

--- models/User.model.js
+++ models/User.model.js
@@ -2,7 +2,8 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
-  username: {
+  email: {
+    required: true,
     type: String,
     unique: true,
   },
--- routes/profile.js
+++ routes/profile.js
@@ -1,3 +1,45 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/User.model");
+
+router.get("/profile", (req, res) => {
+  if (!req.session.user) {
+    return res.redirect("/auth/login");
+  }
+  User.findById(req.session.user._id).then((myUser) => {
+    console.log("myUser:", myUser);
+    res.render("profile");
+  });
+});
+
+router.post("/update-password", (req, res) => {
+  const { oldPassword, newPassword, confirmPassword } = req.body;
+
+  if (newPassword !== confirmPassword) {
+    //   stop and wait a minute
+  }
+  const isSamePassword = bcrypt.compareSync(
+    oldPassword,
+    req.session.user.password
+  );
+
+  if (!isSamePassword) {
+    //   ERROR HANDLING HERE
+  }
+
+  const hashingAlgorithm = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(newPassword, hashingAlgorithm);
+
+  User.findByIdAndUpdate(
+    req.session.user._id,
+    { password: hashedPassword },
+    { new: true }
+  ).then((newAndUpdatedUser) => {
+    req.session.user = newAndUpdatedUser;
+    res.render("profile", {
+      message: "All good, successful, move away",
+    });
+  });
+});
 
 module.exports = router;
