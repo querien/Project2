018453712323d65762aedcc0e384dff7138576ec
Querien <querienornstein@gmail.comm>
2020-11-24 12:02:34 +0100

    added navbar without styling and optimized login

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,6 +4,7 @@ const searchBar = document.forms['search-recipes'].querySelector('input');
 searchBar.addEventListener('keyup', function(e) => {
   const term = e.target.value.toLowerCase();
   const recipes = list.getElementsByTagName('li');
+  
   Array.from(recipes).forEach(recipe  => {
     const title = recipe.firstElementChild.textContent;
     if(title.toLowerCase().indexOf(term) !== -1){
